cc5c09137099bd6ef607ba197a829685780aba69
gnipoleved <oee2010@gmail.com>
2018-04-23 01:18:21 +0900

    ondev - 1st

--- Model/RenamerModel.cs
+++ Model/RenamerModel.cs
@@ -1,17 +1,38 @@
 ﻿
+using System.IO;
 namespace Renamer.Model
 {
     public interface IModel
     {
+        string SelectedDirectory { get; set; }
+
         void Init();
     }
 
 
     public class RenamerModel : IModel
     {
+        private Properties props;
+
+        public /*override*/ string SelectedDirectory { get; set; }
+
+        public RenamerModel()
+        {
+            props = new Properties("renamer.ini");
+        }
+
         void IModel.Init()
         {
-            
+            string readDir = props.get("last_dir");
+            if (string.IsNullOrEmpty(readDir) || File.Exists(readDir) == false)
+            {
+                SelectedDirectory = @"C:\";
+            }
+            else
+            {
+                SelectedDirectory = readDir;
+            }
         }
+
     }
 }
